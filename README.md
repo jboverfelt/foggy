b3ace8e73268ee34a8ca387229f3eb6987369fba
Justin Overfelt <joverfelt@gmail.com>
2013-11-19 21:22:05 -0500

    basic fog score can now be calculated

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # foggy
 
 A Clojure library designed to calculate the Gunning-Fog index of a
-short block of text.
+short block of text. It attempts to keep the main functions
+composable and free of dependencies on one another. However,
+a basic-fog-score function is provided that utilizes default
+helper functions.
 
 ## Usage
 
-(index block-of-text)
+(basic-fog-score block-of-text)
+
+NOTE: The default syllable parsing function is very rough
+and will miscalculate for certain English words that don't
+conform to the basic pattern of vowel groupings
 
 ## License
 
--- src/foggy_text/core.clj
+++ src/foggy_text/core.clj
@@ -1,16 +1,58 @@
 (ns foggy.core
   (:require [clojure.string :as s]))
 
+; basic parsing functions
+
 (defn words [text]
   (-> text
-      (s/replace #"\.|,|;|\?|!" "")
+      (s/replace #"\.|,|;|\?|!\"'" "")
       (s/split #" ")))
 
 (defn sentences [text]
   (re-seq #"\.|!|\?" text))
 
-(defn index [text]
-  (let [word-count (count (words text))
-        sentence-count (count (sentences text))]
-    (/ word-count sentence-count)))
+(defn simple-syllables
+  "This calculates syllables by splitting
+   on vowel groupings. NOTE: This will miss several
+   special cases in English"
+  [word]
+  (filter #(not= %1 "") (s/split word #"[^aeiouy]+")))
 
+(defn complex-words
+  "Given some text, a function that splits
+   a word into a sequence of syllables, and a function
+   that splits text into words, return a sequence
+   of the words which have greater than or equal to 3 syllables"
+  [text syllables words]
+  (filter #(> (count (syllables %1)) 3) (words text)))
+
+; default counting and calculation functions
+
+(defn complex-words-count [complex-words]
+  (count complex-words))
+
+(defn words-count [words]
+  (count words))
+
+(defn sentences-count [sentences]
+  (count sentences))
+
+(defn complex-words-percentage [complex-words words]
+  (/ complex-words words))
+
+(defn words-per-sentence [words sentences]
+  (/ words sentences))
+
+; main functions
+
+(defn fog-index
+  "Given the ratio of complex words to words and the
+   ratio of words per sentence, calculate the Gunning-Fog index"
+  [words-per-sentence complex-words-percentage]
+    (* 0.4
+       (+ words-per-sentence (* 100 complex-words-percentage))))
+
+(defn basic-fog-score [text]
+  (let [complex (complex-words-percentage (complex-words-count (complex-words text simple-syllables words)) (words-count (words text)))
+        words-per (words-per-sentence (words-count (words text)) (sentences-count (sentences text)))]
+    (fog-index words-per complex)))
