957432381b497b634e2a5086e69b9ed81a5bdd2e
Justin Overfelt <joverfelt@gmail.com>
2013-11-19 21:25:46 -0500

    renaming directory structure to match namespace

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject foggy "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure implementation of the Gunning-Fog index"
+  :url "https://github.com/jboverfelt/foggy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
