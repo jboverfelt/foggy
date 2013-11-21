1ce47f549906a4cdf83cf4fc142bf6cefb5a8ff3
Justin Overfelt <joverfelt@gmail.com>
2013-11-21 18:50:14 -0500

    fixing test name

--- test/foggy/core_test.clj
+++ test/foggy/core_test.clj
@@ -5,7 +5,7 @@
 (def paragraph "This has multiple sentences. It's cool! But 
                what does it all mean?")
 
-(deftest a-test
+(deftest basic
   (testing "basic parsing functions"
     (is (= (count (words "This has four words.")) 4))
     (is (= (count (sentences paragraph)) 3))
